0d90ffaaa4f86785083e7e0e39ea7fe03ec48e1f
Procentguide2 <99178092+Procentguide2@users.noreply.github.com>
2024-12-01 16:14:27 +0200

    Update user_register.sql

--- Laboratory-work-9/user_register.sql
+++ Laboratory-work-9/user_register.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION user_register(input_password TEXT)
 RETURNS BOOLEAN AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM user_pass_combo_jay WHERE password = input_password) THEN
-        RAISE EXCEPTION 'Password is too weak. It matches a password in the worst-passwords list.';
+        RAISE EXCEPTION 'Password is too weak. It matches a password in the user_pass_combo_jay list.';
         RETURN FALSE;
     ELSE
         RETURN TRUE;
